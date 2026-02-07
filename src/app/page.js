ee3deba24f78540fde69743a63d59deb45dc54e5
RodryOsyp <94194456+RodryOsyp@users.noreply.github.com>
2026-02-07 11:13:24 +0200

    111

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className={styles.intro}>
+<div className={styles.intro}>
           <h1>To get started, edit the page.js file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
