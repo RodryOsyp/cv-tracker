08ee96b01b1f04c261cdbd7d3ef4afc125c6fc64
TimPlay <osypenkotymur@gmail.com>
2026-02-21 11:01:49 +0200

    fix def

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { Input } from "@/components/ui/input";
 import styles from "./page.module.css";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.page}>
-     <div className="bg-red-500">
-     <h1 className="">Welcome to the CV Tracker</h1>
-     <Button>Click me</Button>
+     <div className="flex flex-col items-center w-screen h-screen justify-center gap-10">
+     <h1 className="text-3xl font-bold ">Welcome to the CV Tracker</h1>
+     <Link href="/login"><Button>Login</Button></Link>
      </div>
     </div>
   );
