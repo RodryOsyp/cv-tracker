4ef9a1bb57431ec88d7130d152708ddc90078889
TimPlay <osypenkotymur@gmail.com>
2026-02-28 11:01:12 +0200

    EndLogin

--- cv-tracker/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Form } from "./components/form";
-
-export default function LoginPage() {
-  return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <Form />
-    </div>
-  );
-}
